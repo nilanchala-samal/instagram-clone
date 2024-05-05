010cf8ffee335b7ae9737b506c10601f20505089
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-05-05 10:56:33 +0530

    added the layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { Button } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage/HomePage"
+import AuthPage from "./pages/AuthPage/AuthPage"
+import PageLayout from "./Layouts/PageLayout/PageLayout"
 
 function App() {
-  
+
   return (
-    <>
-      <Button>Hello World</Button>
-    </>
+    <PageLayout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </PageLayout>
   )
 }
 
