4ae0fea7c5d0f2d3966bfc254a813395b07fa1f1
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-05-07 20:32:26 +0530

    edit profile modal completed and connected to backend

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,12 +1,15 @@
-import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
+import { Avatar, AvatarGroup, Button, Flex, Text, VStack, useDisclosure } from "@chakra-ui/react"
 import useUserProfileStore from "../../store/userProfileStore"
 import useAuthStore from "../../store/authStore";
+import EditProfile from "./EditProfile";
 
 const ProfileHeader = () => {
     const { userProfile } = useUserProfileStore();
     const authUser = useAuthStore((state) => state.user);
     const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username
     const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     return (
         <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: "column", sm: "row" }}>
@@ -31,6 +34,7 @@ const ProfileHeader = () => {
                                 color={"black"}
                                 _hover={{ bg: "whiteAlpha.800" }}
                                 size={{ base: "xs", md: "sm" }}
+                                onClick={onOpen}
                             >
                                 Edit Profile
                             </Button>
@@ -81,7 +85,7 @@ const ProfileHeader = () => {
                 </Flex>
                 <Text fontSize={"sm"}>{userProfile.bio}</Text>
             </VStack>
-
+            {isOpen && <EditProfile isOpen={isOpen} onClose={onClose}/>}
         </Flex>
     )
 }
