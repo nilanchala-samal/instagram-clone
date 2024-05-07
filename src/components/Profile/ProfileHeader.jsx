2f094960abb31d163469eb91f4fffade829a455f
Nilanchala Samal <nilanchala.samal00@gmail.com>
2024-05-07 22:33:06 +0530

    added functionality to follow & unfollow button in any user profile page

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -2,15 +2,16 @@ import { Avatar, AvatarGroup, Button, Flex, Text, VStack, useDisclosure } from "
 import useUserProfileStore from "../../store/userProfileStore"
 import useAuthStore from "../../store/authStore";
 import EditProfile from "./EditProfile";
+import useFollowUser from "../../hooks/useFollowUser";
 
 const ProfileHeader = () => {
     const { userProfile } = useUserProfileStore();
     const authUser = useAuthStore((state) => state.user);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const {isFollowing, isUpdating, handleFollowUser} = useFollowUser(userProfile?.uid);
+    
     const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username
     const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username
-
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
     return (
         <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: "column", sm: "row" }}>
             <AvatarGroup size={{ base: "xl", md: "2xl" }} justifySelf={"center"} alignSelf={"flex-start"} mx={"auto"}>
@@ -47,11 +48,14 @@ const ProfileHeader = () => {
                                 color={"white"}
                                 _hover={{ bg: "blue.6 00" }}
                                 size={{ base: "xs", md: "sm" }}
+                                onClick={handleFollowUser}
+                                isLoading={isUpdating}
                             >
-                                Follow
+                                {isFollowing ? "Unfollow" : "Follow"}
                             </Button>
                         </Flex>
                     )}
+
 
 
 
